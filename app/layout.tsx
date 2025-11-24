b9c7012c4934607927f1d8ce1a3003074bde308e
barudkhan <136376092+barudkhan@users.noreply.github.com>
2025-11-24 08:29:59 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body style={{ margin: 0, background: '#FFD700', minHeight: '100vh' }}>
         {children}
       </body>
     </html>
-  )
+  );
 }

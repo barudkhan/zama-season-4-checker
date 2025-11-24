773aa6ee3151d3974a6530a994266841715b78f5
barudkhan <136376092+barudkhan@users.noreply.github.com>
2025-11-24 07:32:26 +0600

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, background: '#000', color: '#fff', fontFamily: 'system-ui' }}>
-        {children}
-      </body>
-    </html>
-  )
-}

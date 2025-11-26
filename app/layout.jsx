3603297ed772dc7fea91289b3dd811b2271bda16
barudkhan <136376092+barudkhan@users.noreply.github.com>
2025-11-26 15:47:04 +0600

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Zama Checker",
+  description: "Check Twitter rank of Zama users"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
